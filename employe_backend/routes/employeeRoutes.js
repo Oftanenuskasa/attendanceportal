1b4f4fee49d6513874a2b6d0fc66cdc5f89793ec
Oftanenuskasa <oftanenuskasa@gmail.com>
2025-03-07 14:14:21 +0300

    backend changed

--- employe_backend/routes/employeeRoutes.js
+++ employe_backend/routes/employeeRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const Employee = require('../models/Employee');
 const Attendance = require('../models/Attendance');
 const upload = require('../middleware/upload');
